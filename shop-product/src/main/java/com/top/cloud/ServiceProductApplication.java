7a98d992602f41991cabfa2d375f8f12c1755dad
tomaslisheng <tomaslisheng@163.com>
2021-05-21 11:07:24 +0800

    product启动类

---
+++ shop-product/src/main/java/com/top/cloud/ServiceProductApplication.java
@@ -0,0 +1,16 @@
+package com.top.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * author lisheng
+ * Date:2021/5/21
+ * Description:
+ */
+@SpringBootApplication
+public class ServiceProductApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceProductApplication.class,args);
+    }
+}
