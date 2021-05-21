764da4d1177cfbd29bdd2cd6c2b622827b2eda6b
tomaslisheng <tomaslisheng@163.com>
2021-05-21 11:13:42 +0800

    启动类添加和配置文件

---
+++ shop-order/src/main/java/com/top/cloud/ServiceOrderApplication.java
@@ -0,0 +1,16 @@
+package com.top.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * author lisheng
+ * Date:2021/5/21
+ * Description:
+ */
+@SpringBootApplication
+public class ServiceOrderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceOrderApplication.class,args);
+    }
+}
---
+++ shop-user/src/main/java/com/top/cloud/ServiceUserApplication.java
@@ -0,0 +1,16 @@
+package com.top.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * author lisheng
+ * Date:2021/5/21
+ * Description:
+ */
+@SpringBootApplication
+public class ServiceUserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceUserApplication.class,args);
+    }
+}
